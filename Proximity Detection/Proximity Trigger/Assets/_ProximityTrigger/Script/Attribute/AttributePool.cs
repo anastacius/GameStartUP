9b7fa8ac8505900ccdc694441e411ef93a051f8c
Bruno Mikoski <badawe@gmail.com>
2015-11-01 10:37:22 +0100

    Attributes system

---
+++ Proximity Detection/Proximity Trigger/Assets/_ProximityTrigger/Script/Attribute/AttributePool.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gameplay.Attribute
+{
+    public class AttributePool : MonoBehaviour
+    {
+        private Dictionary<AttributeType, Attribute> attributes = new Dictionary<AttributeType, Attribute>();
+
+        private void Awake()
+        {
+            Attribute[] childAttributes = transform.GetComponentsInChildren<Attribute>();
+            attributes = new Dictionary<AttributeType, Attribute>(childAttributes.Length);
+            foreach (Attribute childAttribute in childAttributes)
+            {
+                if (attributes.ContainsKey(childAttribute.AttributeType))
+                {
+                    Debug.Log("Duplicated attribute " + childAttribute.AttributeType, this);
+                    continue;
+                }
+                attributes.Add(childAttribute.AttributeType, childAttribute);
+            }
+        }
+
+        public Attribute GetAttribute(AttributeType targetAttribute)
+        {
+            if (!attributes.ContainsKey(targetAttribute))
+            {
+                Debug.Log("Can't found any attribute of type: " + targetAttribute, this);
+                return null;
+            }
+            return attributes[targetAttribute];
+        }
+    }
+}
--- Proximity Detection/Proximity Trigger/Assets/_ProximityTrigger/Script/Unit/BaseEnemy.cs
+++ Proximity Detection/Proximity Trigger/Assets/_ProximityTrigger/Script/Unit/BaseEnemy.cs
@@ -1,36 +1,76 @@
-﻿using System;
-using UnityEngine;
-using System.Collections;
+﻿using UnityEngine;
+using Gameplay.Attribute;
 using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(NavMeshAgentController))]
 public class BaseEnemy : MonoBehaviour
 {
     private NavMeshAgentController navMeshAgentController;
+    private AttributePool attributePool;
+
+    [SerializeField]
+    private bool remove10Health;
+
+    [SerializeField]
+    private bool increaseArmorBy1;
 
     private void Awake()
     {
         navMeshAgentController = GetComponent<NavMeshAgentController>();
+        attributePool = GetComponentInChildren<AttributePool>();
     }
 
     private void Start()
     {
         SeekNewPosition();
+
+        attributePool.GetAttribute(AttributeType.Health).Initialize(100, 100);
+        attributePool.GetAttribute(AttributeType.Armor).Initialize(2, 10);
+        attributePool.GetAttribute(AttributeType.MoveSpeed).Initialize(4, 10);
+
+        attributePool.GetAttribute(AttributeType.Health).OnAttributeOver += OnHealthOver;
     }
 
     private void OnEnable()
     {
         navMeshAgentController.OnReachDestination += SeekNewPosition;
+
+        
+
     }
+
+    
 
     private void OnDisable()
     {
         navMeshAgentController.OnReachDestination -= SeekNewPosition;
+
+        attributePool.GetAttribute(AttributeType.Health).OnAttributeOver -= OnHealthOver;
+    }
+
+    private void OnHealthOver(float prevValue, float currentValue)
+    {
+        Destroy(this.gameObject);
     }
 
     private void SeekNewPosition()
     {
         Vector3 randomPosition = new Vector3(Random.Range(-50,50), 0 , Random.Range(-50, 50));
         navMeshAgentController.SetDestination(randomPosition);
+    }
+
+    private void Update()
+    {
+        if (remove10Health)
+        {
+            attributePool.GetAttribute(AttributeType.Health).ChangeValue(-10);
+            remove10Health = false;
+        }
+
+        if (increaseArmorBy1)
+        {
+            attributePool.GetAttribute(AttributeType.Armor).ChangeValue(1);
+            increaseArmorBy1 = false;
+        }
     }
 }
